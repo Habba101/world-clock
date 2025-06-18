745df1a52a804630264047e3c2b9f1de74ffb856
Habba101 <rahabamotaung@yahoo.com>
2025-06-18 14:18:24 +0200

    Added JavaScript and updated HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  let copenhagenElement = document.querySelector("#copenhagen");
+  let copenhagenDateElement = copenhagenElement.querySelector(".date");
+  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  let copenhagenTime = moment().tz("Europe/Copenhagen");
 
+  copenhagenDateElement.innerHTML = copenhagenTime.format("MMMM Do YYYY");
+  copenhagenTimeElement.innerHTML = copenhagenTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let dubaiElement = document.querySelector("#dubai");
+  let dubaiDateElement = dubaiElement.querySelector(".date");
+  let dubaiTimeElement = dubaiElement.querySelector(".time");
+  let dubaiTime = moment().tz("Asia/Dubai");
+
+  dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do YYYY");
+  dubaiTimeElement.innerHTML = dubaiTime.format("h:mm:ss [<small>]A[</small>]");
+
+  let melbourneElement = document.querySelector("#melbourne");
+  let melbourneDateElement = melbourneElement.querySelector(".date");
+  let melbourneTimeElement = melbourneElement.querySelector(".time");
+  let melbourneTime = moment().tz("Australia/Melbourne");
+
+  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+  melbourneTimeElement.innerHTML = melbourneTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
